94bcbe44c080d3fbc1d2b25973da198946038539
majkus522 <mikohus522@gmail.com>
2022-10-04 12:09:48 +0200

    Init

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 
 namespace KalkulatorWynajmu.Controllers
 {
@@ -21,41 +22,20 @@ namespace KalkulatorWynajmu.Controllers
 		}
 
 		[HttpGet]
-		public ActionResult<string> Get([FromQuery]int distance, [FromQuery] int year, [FromQuery] DateTime start, [FromQuery] DateTime end, [FromQuery] CarClass carClass)
+		public IActionResult Get([FromQuery] int distance, [FromQuery] int year, [FromQuery] DateTime start, [FromQuery] DateTime end, [FromQuery] CarClass carClass)
 		{
 			DateTime today = DateTime.Today;
-			if (today > start)
-			{
-				return Ok("Niepoprawna data rozpoczęcia wynajmu");
-			}
-			if(today > end || end < start)
-			{
-				return Ok("Niepoprawna data zakończenia wynajmu");
-			}
-			if(distance < 0)
-			{
-				return Ok("Niepoprawna odległość");
-			}
-			if (year < 1900)
-			{
-				return Ok("Niepoprawny rok wydania prawa jazdy");
-			}
-
 			float fuelPrice = 6.96f;
 			float basePrice = 40;
 			float fuelConsumption = 7.5f;
 			int modelAvaliable = 1;
 
-			if(today.Year - year < 3 && carClass == CarClass.Premium)
-				return Ok("Nie możesz wyporzyczyć tego pojazdu");
-
 			int days = end.Subtract(start).Days;
 			float daysCost = basePrice * days;
-			string placeholder = "Cena bazowa (" + days + " " + (days == 1 ? "dzień" : "dni") + "):\t\t" + daysCost + " zł\r\n";
 			float finalResult = daysCost;
 
 			float carClassCost = 0;
-			switch(carClass)
+			switch (carClass)
 			{
 				case CarClass.Standard:
 					carClassCost = .3f * daysCost;
@@ -70,30 +50,32 @@ namespace KalkulatorWynajmu.Controllers
 					break;
 			}
 			finalResult += carClassCost;
-			placeholder += "Cena klasy pojazdu:\t\t\t" + carClassCost + " zł\r\n";
 
+			float expirienceCost = 0;
 			if (today.Year - year < 5)
 			{
-				float expirienceCost = .2f * finalResult;
-				placeholder += "Niedoświadczony kierowca:\t" + expirienceCost + " zł\r\n";
+				expirienceCost = .2f * finalResult;
 				finalResult += expirienceCost;
 			}
 
+			float modelCountCost = 0;
 			if (modelAvaliable < 3)
 			{
-				float modelCountCost = .15f * finalResult;
-				placeholder += "Mała ilośc pojazdów:\t\t" + modelCountCost + " zł\r\n";
+				modelCountCost = .15f * finalResult;
 				finalResult += modelCountCost;
 			}
 
 			float fuelCost = distance / 100 * fuelConsumption * fuelPrice;
-			placeholder += "Cena paliwa:\t\t\t\t" + fuelCost + " zł\r\n";
 			finalResult += fuelCost;
 
-			placeholder += "---------------------------------------\r\n";
-			placeholder += "Razem:\t\t\t\t\t\t" + finalResult + " zł";
-
-			return Ok(placeholder);
+			return new JsonResult(new Dictionary<string, float>()
+			{
+				{ "baseCost", daysCost},
+				{ "expirienceCost", expirienceCost},
+				{ "modelCountCost", modelCountCost},
+				{ "fuelCost", fuelCost},
+				{ "sum", finalResult}
+			});
 		}
 	}
 }
