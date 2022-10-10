f7bd25dfd43da9e1e650d34343d83c122b961e4a
majkus522 <mikohus522@gmail.com>
2022-10-10 11:46:56 +0200

    Model warstwowy API

---
+++ DataAcces/DatabaseContext.cs
@@ -0,0 +1,57 @@
+﻿using Common;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAcces
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<Car> Cars { get; set; }
+
+		string connection = "Server=(localdb)\\mssqllocaldb;Database=CrasDB;Trusted_Connection=True;";
+
+		public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Car>().HasData(new Car[]
+			{
+				new Car()
+				{
+					Id = 1,
+					Brand = "Skoda",
+					Model = "KAMIQ",
+					CarAvaliable = 10,
+					BasePrice = 36.99f,
+					FuelConsumption = 5.81f,
+					CarClass = ECarClass.Standard
+				},
+				new Car()
+				{
+					Id = 2,
+					Brand = "BMW",
+					Model = "M3 TOURING",
+					CarAvaliable = 1,
+					BasePrice = 69.99f,
+					FuelConsumption = 10.4f,
+					CarClass = ECarClass.Premium
+				},
+				new Car()
+				{
+					Id = 3,
+					Brand = "Audi",
+					Model = "A1 Sportback",
+					CarAvaliable = 1,
+					BasePrice = 45.99f,
+					FuelConsumption = 5.6f,
+					CarClass = ECarClass.Medium
+				}
+			});
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(connection);
+		}
+	}
+}
---
+++ Infrastructure/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using DataAcces;
+using Domain;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+	public interface ICalculator
+	{
+		public Task<Result> getCars(DatabaseContext context);
+		public Task<Result> get(int id, InputData Data, DatabaseContext context);
+		public Task<Result> put(int id, Car car, DatabaseContext context);
+		public Task postCar(Car car, DatabaseContext context);
+	}
+}
